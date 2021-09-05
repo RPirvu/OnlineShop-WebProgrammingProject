af3c2f083e32f103d94c952b394de9243512193c
Robert <62704626+RPirvu@users.noreply.github.com>
2021-09-05 19:39:57 +0300

    cart routes

--- routes/cart.php
+++ routes/cart.php
@@ -23,32 +23,142 @@ $app->post('/addCart', function(Request $request, Response $response){
 });
 
 $app->delete('/deleteCart', function(Request $request, Response $response){
-    $db = new Database();
-    $conn = $db->open();
 
     $userid = 		$request->getParam('id');
 	$quantity = 	$request->getParam('quantity');
 	$product_id = 	$request->getParam('product_id');
 
-	$sql = $conn->prepare("DELETE FROM cart WHERE user_id=:userid AND quantity=:quantity AND product_id=:product_id");
+	$sql = "DELETE FROM cart WHERE user_id=$userid AND product_id=$product_id";
 
-	return "success";
+	try {
+        $db = new Database();
+        $conn = $db->open();
+
+        $stmt = $conn->query($sql);
+    
+        $db->close();
+        $response = "success";
+        return $response;
+    } catch (PDOException $e){
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+    
 });
 
-// $app->get('/detailsCart', function(Request $request, Response $response){
-// 	$db = new Database();
-//     $conn = $db->open();
+$app->get('/detailsCart', function(Request $request, Response $response){
 
-// 	$userid = 		$request->getParam('id');
-// 	$product_id = 	$request->getParam('product_id');
+	$userid = $request->getParam('id');
 
-// // $app->get('/allCart', function(Request $request, Response $response){
+	$sql = "SELECT product_id FROM cart WHERE user_id=$userid";
+
+	try {
+        $db = new Database();
+        $conn = $db->open();
+
+        $stmt = $conn->query($sql);
+		$cartdetails = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $response->getBody()->write(json_encode($cartdetails));
+
+		// foreach($aux as $details) {
+		// 	$stmt = $conn->prepare("SELECT *, cart.id AS cartid FROM cart LEFT JOIN products ON products.id=cart.product_id AND products.id=$details WHERE user_id=:user");
+		// 	$stmt->execute(['user'=>$userid]);
+
+		// 	$produs = $stmt->fetch(PDO::FETCHOBJ);
+
+		// 	$response->getBody()->write(json_encode($produs));
+			
+		// }
+    
+
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200); 
+    } catch (PDOException $e){
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+    
+});
+
+// $app->get('/allCart', function(Request $request, Response $response){
+	// $sql = "SELECT * FROM cart LEFT JOIN products ON products.id=cart.product_id LEFT JOIN category ON category.id=products.category_id WHERE user_id=:$userid";
+// });
+
+$app->get('/totalCart', function(Request $request, Response $response){ 
+
+	$userid = $request->getParam('id');
+
+	$sql = "SELECT * FROM cart LEFT JOIN products on products.id=cart.product_id WHERE user_id=$userid";
+	
+	try {
+        $db = new Database();
+        $conn = $db->open();
+
+        $stmt = $conn->query($sql);
+		$allcart = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $response->getBody()->write(json_encode($allcart));
+
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200); 
+    } catch (PDOException $e){
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+    
+});
 
 
-// $app->get('/totalCart', function(Request $request, Response $response){
 
 
-// // $app->update('/updateCart', function(Request $request, Response $response){
+$app->put('/updateCart', function(Request $request, Response $response){
+
+
+	$userid = $request->getParam('id');
+	$quantity = $request->getParam('quantity');
+
+
+	try {
+        $db = new Database();
+        $conn = $db->open();
+
+		$stmt = $conn->prepare("UPDATE cart SET quantity=:quantity WHERE id=:id");
+		$stmt->execute(['quantity'=>$quantity, 'id'=>$userid]);
+		
+        $response = "success";
+
+        return $response;
+    } catch (PDOException $e){
+        $error = array(
+            "message" => $e->getMessage()
+        );
+        
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+    
+});
 
 
 
